9891a74c2d56d0b6765028367bd90eb9823713c2
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-02-09 20:35:15 -0600

    FP Controller
    First Person Controller Integration
    Terrain Tweaks
    Probuilder Tweaks

---
+++ AGES_FPN/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    Rigidbody rb;
+    public bool isGrounded = true;
+
+    public float tempjumptimer = 0;
+    //TEMPJUMPTIMER is a placeholder
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (isGrounded == false)
+        {
+            tempjumptimer++;
+
+            if (tempjumptimer >= 60)
+            {
+                isGrounded = true;
+                tempjumptimer = 0;
+            }
+        }
+
+    }
+
+}
